d286305bea096878c67f6150df5d6539b06bceef
lukas <bierling.lukas@gmail.com>
2025-05-01 00:54:18 +0200

    removed enseble bug

--- dreamerv3/agent.py
+++ dreamerv3/agent.py
@@ -84,8 +84,6 @@ class Agent(embodied.jax.Agent):
 
     self.priority_collector = {}
 
-    self.ensemble = [rssm.RSSM(name="dyn" + str(i), **config.dyn[config.dyn.typ]) for i in range(5)]
-
   @property
   def policy_keys(self):
     return '^(enc|dyn|dec|pol)/'

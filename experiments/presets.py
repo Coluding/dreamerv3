d4b6b67cdc8e5bf5fe92fca89f1e4c0fb86a84ca
--global <janhenrik.bertrand@gmail.com>
2025-05-10 19:56:33 +0200

    First experiment definitions

--- experiments/presets.py
+++ experiments/presets.py
@@ -1,8 +1,26 @@
 DEFAULT_RUN_CFG = {
     "logdir": r"~/logdir/{timestamp}", 
-    "configs": "atari100k", 
+    "configs": "atari100k",
     "run.train_ratio": 32,
     "run.duration": 120,
     "run.steps": 2000,
+}
+
+REPLAY_BUFFER_RUN_CFG = {
+    "logdir": r"~/logdir/{timestamp}", 
+    "configs": "atari100k",
+    "run.train_ratio": 32,
+    "run.duration": 120,
+    "run.steps": 2000,
+    "replay_context": 1,
+}
+
+REPLAY_LATENT_DISAGREEMENT_CFG = {
+    "logdir": r"~/logdir/{timestamp}", 
+    "configs": "atari100k",
+    "run.train_ratio": 32,
+    "run.duration": 120,
+    "run.steps": 2000,
+    "replay_context": 0,
 }
 

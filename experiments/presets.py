0f7b5dc75177167e1ecde96c3ca950f6d49e67cf
--global <janhenrik.bertrand@gmail.com>
2025-05-21 21:57:26 +0200

    Updated config

--- experiments/presets.py
+++ experiments/presets.py
@@ -1,6 +1,8 @@
 DEFAULT_RUN_CFG = {
     "logdir": r"~/logdir/default/{timestamp}",
     "run.train_ratio": 32,
+    "run.log_every": 10,
+    "run.save_every": 2000,
     # "run.duration": 120,
     # "run.steps": 2000,
 }
@@ -8,6 +10,8 @@ DEFAULT_RUN_CFG = {
 REPLAY_BUFFER_RUN_CFG = {
     "logdir": r"~/logdir/optimized_replay_buffer/{timestamp}", 
     "run.train_ratio": 32,
+    "run.log_every": 10,
+    "run.save_every": 2000,
     # "run.duration": 120,
     # "run.steps": 2000,
     # "replay.fracs": {"uniform": 0.0, "priority": 1.0, "recency": 0.0},
@@ -25,11 +29,13 @@ REPLAY_BUFFER_RUN_CFG = {
 REPLAY_LATENT_DISAGREEMENT_CFG = {
     "logdir": r"~/logdir/latent_reward_disagreement/{timestamp}", 
     "run.train_ratio": 32,
-    "run.duration": 0,
-    "run.steps": 4000,
+    "run.log_every": 10,
+    "run.save_every": 2000,
+    # "run.duration": 0,
+    # "run.steps": 4000,
     "replay_context": 0,
     "agent.use_intrinsic": True,
-    "agent.intrinsic.learn_strategy": "perturbed_starts", # ema, joint, perturbed_starts
+    "agent.intrinsic.learn_strategy": "joint_mlp", # ema, joint, perturbed_starts
     "agent.intrinsic.exploration_type": "reward_variance", # state_disagreement, reward_variance
     "agent.intrinsic.reward_type": "disagreement", # prediction_error, disagreement, max_disagreement
 }

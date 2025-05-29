3c4e62443d288b70722030a825efc43e3159de3c
--global <janhenrik.bertrand@gmail.com>
2025-05-29 12:20:25 +0200

    Added scheduling presets for the intrinsic lambda

--- experiments/presets.py
+++ experiments/presets.py
@@ -2,7 +2,7 @@ DEFAULT_RUN_CFG = {
     "logdir": r"~/logdir/default/{timestamp}",
     "run.train_ratio": 32,
     "run.log_every": 10,
-    "run.save_every": 2000,
+    "run.save_every": 10000,
     # "run.duration": 120,
     # "run.steps": 2000,
 }
@@ -11,7 +11,7 @@ REPLAY_BUFFER_RUN_CFG = {
     "logdir": r"~/logdir/optimized_replay_buffer/{timestamp}", 
     "run.train_ratio": 32,
     "run.log_every": 10,
-    "run.save_every": 2000,
+    "run.save_every": 10000,
     # "run.duration": 120,
     # "run.steps": 2000,
     # "replay.fracs": {"uniform": 0.0, "priority": 1.0, "recency": 0.0},
@@ -30,7 +30,21 @@ REPLAY_LATENT_DISAGREEMENT_CFG = {
     "logdir": r"~/logdir/latent_reward_disagreement/{timestamp}", 
     "run.train_ratio": 32,
     "run.log_every": 10,
-    "run.save_every": 2000,
+    "run.save_every": 10000,
+    # "run.duration": 0,
+    # "run.steps": 4000,
+    "replay_context": 0,
+    "agent.use_intrinsic": True,
+    "agent.intrinsic.learn_strategy": "joint_mlp", # ema, joint, perturbed_starts
+    "agent.intrinsic.exploration_type": "state_disagreement", # state_disagreement, reward_variance
+    "agent.intrinsic.reward_type": "disagreement", # prediction_error, disagreement, max_disagreement
+}
+
+REPLAY_LATENT_DISAGREEMENT_CFG_EMA = {
+    "logdir": r"~/logdir/latent_reward_disagreement/{timestamp}", 
+    "run.train_ratio": 32,
+    "run.log_every": 10,
+    "run.save_every": 10000,
     # "run.duration": 0,
     # "run.steps": 4000,
     "replay_context": 0,
@@ -38,6 +52,22 @@ REPLAY_LATENT_DISAGREEMENT_CFG = {
     "agent.intrinsic.learn_strategy": "joint_mlp", # ema, joint, perturbed_starts
     "agent.intrinsic.exploration_type": "reward_variance", # state_disagreement, reward_variance
     "agent.intrinsic.reward_type": "disagreement", # prediction_error, disagreement, max_disagreement
+    "agent.intrinsic.scheduling_strategy": "slope_ema",
+}
+
+REPLAY_LATENT_DISAGREEMENT_CFG_EXP_DECAY = {
+    "logdir": r"~/logdir/latent_reward_disagreement/{timestamp}", 
+    "run.train_ratio": 32,
+    "run.log_every": 10,
+    "run.save_every": 10000,
+    # "run.duration": 0,
+    # "run.steps": 4000,
+    "replay_context": 0,
+    "agent.use_intrinsic": True,
+    "agent.intrinsic.learn_strategy": "joint_mlp", # ema, joint, perturbed_starts
+    "agent.intrinsic.exploration_type": "reward_variance", # state_disagreement, reward_variance
+    "agent.intrinsic.reward_type": "disagreement", # prediction_error, disagreement, max_disagreement
+    "agent.intrinsic.scheduling_strategy": "exp_decay",
 }
 
 
